4396a791e51ba9ba3d36c500aec7fa322051f189
Anne Ganzeboom <anneganzeboom@gmail.com>
2015-05-06 20:27:20 +0100

    function add and minus works

--- atm.js
+++ atm.js
@@ -1,4 +1,34 @@
+
+var totalbalance1 = 0;
+var deposit = ""
+var accounttotal = ""
+
+var setUpEventListerners = function(){
+  
+  $("#deposit1").click(function(){
+   desposit = parseInt($("#amount1").val());
+   console.log(desposit);
+   totalbalance1 = totalbalance1 + Number(desposit);
+   $("#balance1").html(totalbalance1); 
+  });
+
+
+ $("#withdraw1").click(function(){
+   desposit = parseInt($("#amount1").val());
+   console.log(desposit);
+   totalbalance1 = totalbalance1 - Number(desposit);
+   $("#balance1").html(totalbalance1); 
+  });
+
+}
+
 $(document).ready(function(){
   
-})
+  // call a function with your event listeners
+  setUpEventListerners();
 
+});
+
+ // var accounttotal = balance + desposit;
+ //    var balance = accounttotal;
+ //    $('#balance1').text('$' + accounttotal);
